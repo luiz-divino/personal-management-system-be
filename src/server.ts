4a957f1a07a9d51e47cf8e6dba1e52f2836f97d0
luiz-divino <nandodivinodev@gmail.com>
2026-05-17 01:27:33 -0300

    chore: update host

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import './util/module-alias'
+import "./util/module-alias";
 import "dotenv/config";
-import express from 'express';
-import cors from 'cors';
-import { router } from './routes';
+import express from "express";
+import cors from "cors";
+import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
-app.use(cors())
-app.use(router)
+app.use(cors());
+app.use(router);
 
-const PORT = process.env.PORT || 3333;
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
-})          
+const port = Number(process.env.PORT) || 3333;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
